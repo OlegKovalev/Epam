097d21b583a6fedde71d06ce5ef48b740a1700cc
Oleg <www.olegvip@mail.ru>
2016-07-30 02:28:24 +0300

    1

---
+++ src/se01/CrossMatrix.java
@@ -0,0 +1,18 @@
+package se01;
+
+public class CrossMatrix {
+	public static void main(String[] args){
+		int size = 10;
+
+		for(int i=0; i < size; i++){
+			for(int j=0; j < size; j++){
+				if(i == j || j == size-i-1)
+					System.out.print(1 + "\t");
+				else
+					System.out.print(0 + "\t");
+
+			}
+			System.out.println();
+		}
+	}
+}
