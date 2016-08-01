3576eafd48d57c414472b7f2fef29a2c27df61cc
Oleg <www.olegvip@mail.ru>
2016-08-01 17:52:20 +0300

    fixed MaxOfArray

--- src/se01/task4/MaxOfArray.java
+++ src/se01/task4/MaxOfArray.java
@@ -7,7 +7,7 @@ public class MaxOfArray {
 		double maxValue = Double.MIN_VALUE;
 		int lengthOfArray = arr.length;
 
-		for(int i=0; i < lengthOfArray; i++){
+		for(int i=0; i < lengthOfArray/2; i++){
 			if(arr[i] + arr[lengthOfArray-i-1] > maxValue){
 				maxValue = arr[i] + arr[lengthOfArray-i-1];
 			}
