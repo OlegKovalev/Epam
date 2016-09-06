7e20c7dc41d0cabd51027612795c0f37d3327c05
OlegKovalev <www.olegvip@mail.ru>
2016-09-06 21:10:06 +0300

    update

--- src/se01/task1/Logic.java
+++
@@ -1,7 +0,0 @@
-package se01.task1;
-
-public class Logic {
-    public String method() {
-        return "I am string in Logic.";
-    }
-}
--- src/se01/task1/Main.java
+++
@@ -1,9 +0,0 @@
-package se01.task1;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Logic logic = new Logic();
-        System.out.println(logic.method());
-    }
-}
