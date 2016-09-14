7c485917acb4e82541654ccdebf34930447849d9
OlegKovalev <www.olegvip@mail.ru>
2016-09-14 23:04:40 +0300

    fixed

---
+++ src/Epam/src/test/se05/task2/PropertyReaderTest.java
@@ -0,0 +1,25 @@
+package se05.task2;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PropertyReaderTest {
+    PropertyReader propReader = new PropertyReader("se05.se04.task2.question");
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getKeys() throws Exception {
+
+    }
+
+    @Test
+    public void getValue() throws Exception {
+
+    }
+
+}
