2f35540fce601fd10c062a644c4a568cf8924de8
Oleg <www.olegvip@mail.ru>
2016-10-08 02:32:01 +0300

    update

--- src/main/java/PatternSplitExample.java
+++
@@ -1,8 +0,0 @@
-public class PatternSplitExample {
-    public static void main(String[] args) {
-        
-        String line = "fewbvhewybvweu.";
-        String[] tokens = line.split("\\.");
-        System.out.print(tokens.length);
-    }
-}
